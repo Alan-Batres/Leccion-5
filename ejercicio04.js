1610493986932203175824e12f97a5a35e4975aa
Alan-Batres <alanjoelbatres@gmail.com>
2025-09-24 13:30:14 -0600

    Update ejercicio04.js

--- ejercicio04.js
+++ ejercicio04.js
@@ -1,13 +1,5 @@
 function esImpar(){
     let array = [3, 2, 6, 4, 8];
-    let isOdd = array => {
-        for (let num of array){
-            if (num % 2 == 0){
-                return true
-            } else {
-                return false
-            }
-        }
-    }
-    console.log(isOdd(array));
+    let isOdd = value => value % 2 != 0;
+    console.log(array.some(isOdd));
 }
