3b0db1ef9873514725ece2a9c36173f2383750fa
Alan-Batres <alanjoelbatres@gmail.com>
2025-09-24 13:29:47 -0600

    Update ejercicio03.js

--- ejercicio03.js
+++ ejercicio03.js
@@ -1,13 +1,6 @@
 function EsPar(){
     let array = [2, 4, 6, 7];
-    let isEven = array => {
-        for (let num of array){
-            if(num % 2 != 0){
-                return true;
-            }else{
-                return false
-            }
-        }
-    }
-    console.log(isEven(array));
+    let isEven = value => value % 2 == 0;
+    console.log(array.every(isEven));
 }
+
