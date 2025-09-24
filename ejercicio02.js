b7ccea5f2412609c5b3df6025874dd4b1d17d4ef
Alan-Batres <alanjoelbatres@gmail.com>
2025-09-24 12:34:47 -0600

    Update ejercicio02.js

--- ejercicio02.js
+++ ejercicio02.js
@@ -1,6 +1,7 @@
 function IncluyeValor() {
     const card = 3;
     const array = [2, 3, 4, 5];
-    let includesValue = (card,array) => card in array ? true : false;
+    let includesValue = (card,array) => array.includes(card) ? true : false;
     console.log(includesValue(card,array));
+
 }
